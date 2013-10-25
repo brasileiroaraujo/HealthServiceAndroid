dfc53aa23fcabe2a28aae652d3036d907be6f060
theomoura <theomouram@gmail.com>
2013-10-25 17:43:15 -0300

    notification large icon adjusted

--- src/com/signove/health/servicetest/NotificationReceiver.java
+++ src/com/signove/health/servicetest/NotificationReceiver.java
@@ -1,15 +1,14 @@
 package com.signove.health.servicetest;
 
-import android.app.Notification;
+import android.app.Notification; 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 public class NotificationReceiver extends BroadcastReceiver {
 
@@ -23,12 +22,14 @@ public class NotificationReceiver extends BroadcastReceiver {
 		Intent intent1 = new Intent(context, HealthServiceTestActivity.class);
 		PendingIntent pIntent = PendingIntent.getActivity(context, 0, intent1, 0);
 		
-		Log.w("NOTIFICATION","NOTIFICOU");
+		Bitmap bm = BitmapFactory.decodeResource(context.getResources(), R.drawable.heart_80px);
 		
 		Notification n = new NotificationCompat.Builder(context)
-				.setContentTitle("Lembrete do MyPressure")
-				.setContentText("Clique aqui para medir sua pressao")
-				.setSmallIcon(R.drawable.heart).setContentIntent(pIntent)
+				.setContentTitle("CheckMyPressure Reminder")
+				.setContentText("Click here to check your pressure today")
+				.setSmallIcon(R.drawable.heart)
+				.setLargeIcon(bm)
+				.setContentIntent(pIntent)
 				.build();
 
 		n.flags |= Notification.FLAG_AUTO_CANCEL;

ee3531fc9ffd5f533a75b999714a65eaf43097e0
brasileiroaraujo <brasileiroaraujo@gmail.com>
2013-10-29 10:42:06 -0200

    parser XML ok

--- src/br/ufcg/embedded/health/servicetest/Handlers.java
+++ src/br/ufcg/embedded/health/servicetest/Handlers.java
@@ -32,6 +32,7 @@ public class Handlers {
     private ParserXML parser;
     private Activity frame;
     private List<Double> datas = new ArrayList<Double>();
+    private boolean flag = false;
 
     public Handlers(Map<String, String> map, Activity frame) {
         this.menssage = (TextView) frame.findViewById(R.id.tvMsg);
@@ -86,120 +87,14 @@ public class Handlers {
     }
 
     public void handle_packet_measurement(String path, String xml) {
-        String measurement = "";
         Document document = parser.parse_xml(xml);
 
         if (document == null) {
             return;
         }
 
-        NodeList datalists = document.getElementsByTagName("data-list");
-
-        for (int i = 0; i < datalists.getLength(); ++i) {
-
-            Log.w("Antidote", "processing datalist " + i);
-
-            Node datalist_node = datalists.item(i);
-            NodeList entries = ((Element) datalist_node)
-                    .getElementsByTagName("entry");
-
-            for (int j = 0; j < entries.getLength(); ++j) {
-                
-                Log.w("Antidote", "processing entry " + j);
-
-                boolean ok = false;
-                String unit = "";
-                String value = "";
-
-                Node entry = entries.item(j);
-
-                // scan immediate children to dodge entry inside another entry
-                NodeList entry_children = entry.getChildNodes();
-
-                for (int k = 0; k < entry_children.getLength(); ++k) {
-                    Node entry_child = entry_children.item(k);
-
-                    Log.w("Antidote",
-                            "processing entry child "
-                                    + entry_child.getNodeName());
-                    
-                    
-                    
-                    
-                    if (entry_child.getNodeName().equals("meta-data")) {
-                        // meta-data -> meta name=unit
-                        NodeList metas = ((Element) entry_child)
-                                .getElementsByTagName("meta");
-
-                        Log.w("Antidote",
-                                "meta-data.meta count: " + metas.getLength());
-
-                        for (int l = 0; l < metas.getLength(); ++l) {
-                            Log.w("Antidote", "Processing meta " + l);
-                            NamedNodeMap attr = metas.item(l).getAttributes();
-                            if (attr == null) {
-                                Log.w("Antidote", "Meta has no attributes");
-                                continue;
-                            }
-                            Node item = attr.getNamedItem("name");
-                            if (item == null) {
-                                Log.w("Antidote",
-                                        "Meta has no 'name' attribute");
-                                continue;
-                            }
-
-                            Log.w("Antidote",
-                                    "Meta attr 'name' is "
-                                            + item.getNodeValue());
-
-                            if (item.getNodeValue().equals("unit")) {
-                                Log.w("Antidote", "Processing meta unit");
-                                String text = parser
-                                        .get_xml_text(metas.item(l));
-                                if (text != null) {
-                                    unit = text;
-                                    if (unit.equals("mmHg")){
-                                        if (entry_child.getNodeName().equals("simple")) {
-                                            // simple -> value -> (text)
-                                            NodeList simple = ((Element) entry_child)
-                                                    .getElementsByTagName("value");
-                                            Log.w("Antidote",
-                                                    "simple.value count: " + simple.getLength());
-                                            if (simple.getLength() > 0) {
-                                                String textSimple = parser.get_xml_text(simple.item(0));
-                                                if (textSimple != null) {
-                                                    ok = true;
-                                                    value = textSimple;
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-
-                    }
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-
-                }
-
-                if (ok) {
-                    if (unit != "") {
-                        measurement += value + " " + unit + "\n";
-                    } else {
-                        measurement += value + " ";
-                        Log.w("AntidoteDatabase", unit + "--" + value);
-                        datas.add(Double.parseDouble(value));
-                    }
-                }
-            }
-        }
+        datas = parser.extractValues(document);
+        
         show(data, "SYS: " + datas.get(0).intValue() + "\n" + "DIS: "
                 + datas.get(1).intValue());
         show(menssage,
@@ -257,7 +152,7 @@ public class Handlers {
         if (insert != null && lastInsert != null) {
             long interval = (insert.getDate().getTime() - lastInsert.getDate()
                     .getTime()) / 1000;
-            if (interval > TIMEOUT_PERSISTENCE) {
+            if (interval > TIMEOUT_PERSISTENCE || insert.getSystolic() != lastInsert.getSystolic() || insert.getDiastolic() != lastInsert.getDiastolic()) {
                 return true;
             } else {
                 return false;
--- src/br/ufcg/embedded/health/servicetest/ParserXML.java
+++ src/br/ufcg/embedded/health/servicetest/ParserXML.java
@@ -1,7 +1,10 @@
 package br.ufcg.embedded.health.servicetest;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -48,5 +51,93 @@ public class ParserXML {
         }
 
         return s;
+    }
+    
+    public List<Double> extractValues(Document doc){
+        List<Double> listData = new ArrayList<Double>();
+        try {
+            //optional, but recommended
+            //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("entry");
+
+            for (int i = 0; i < nList.getLength(); i++) {
+                NodeList nListChildEntry = nList.item(i).getChildNodes();
+                for (int j = 0; j < nListChildEntry.getLength(); j++) {
+                    Node nNodeChildEntry = nListChildEntry.item(j);
+                    if(nNodeChildEntry.getNodeName().equals("meta-data")){
+                        NodeList nListChildMetadata = nNodeChildEntry.getChildNodes();
+                        for (int k = 0; k < nListChildMetadata.getLength(); k++) {
+                            Node nNodeChildMetadata = nListChildMetadata.item(k);
+                            if(nNodeChildMetadata.getNodeName().equals("meta")){
+                                NodeList nListChildMeta = nNodeChildMetadata.getChildNodes();
+                                for (int l = 0; l < nListChildMeta.getLength(); l++) {
+                                    if(nListChildMeta.item(l).getNodeValue().equals("mmHg")){
+                                        //Find metadata of pressure(mmHg), now go to entry whith datas.
+                                        for (int m = j+1; m < nListChildEntry.getLength(); m++) {
+                                            Node nNodeChildEntryMetadata = nListChildEntry.item(m);
+                                            if(nNodeChildEntryMetadata.getNodeName().equals("compound")){
+                                                NodeList nListEntryMetadata = nNodeChildEntryMetadata.getChildNodes();
+                                                for (int n = 0; n < nListEntryMetadata.getLength(); n++) {
+                                                    Node nNodeChildCompound = nListEntryMetadata.item(n);
+                                                    if(nNodeChildCompound.getNodeName().equals("entries")){
+                                                        NodeList nListCompound = nNodeChildCompound.getChildNodes();
+                                                        for (int o = 0; o < nListCompound.getLength(); o++) {
+                                                            Node nNodeChildEntries = nListCompound.item(o);
+                                                            if(nNodeChildEntries.getNodeName().equals("entry")){
+                                                                NodeList nListEntry = nNodeChildEntries.getChildNodes();
+                                                                for (int p = 0; p < nListEntry.getLength(); p++) {
+                                                                    Node nNodeChildEntryData = nListEntry.item(p);
+                                                                    if(nNodeChildEntryData.getNodeName().equals("simple")){
+                                                                        NodeList nListSimple = nNodeChildEntryData.getChildNodes();
+                                                                        for (int q = 0; q < nListSimple.getLength(); q++) {
+                                                                            Node nNodeChildSimple = nListSimple.item(q);
+                                                                            if(nNodeChildSimple.getNodeName().equals("value")){
+                                                                                NodeList nListValue = nNodeChildSimple.getChildNodes();
+                                                                                for (int r = 0; r < nListValue.getLength(); r++) {
+                                                                                    Node nNodeValue = nListValue.item(r);
+                                                                                    listData.add(Double.parseDouble(nNodeValue.getNodeValue()));
+                                                                                }
+                                                                                
+                                                                            }
+                                                                            
+                                                                        }
+                                                                        
+                                                                    }
+                                                                    
+                                                                }
+                                                                
+                                                            }
+                                                            
+                                                        }
+                                                        
+                                                    }
+                                                    
+                                                }
+                                                
+                                            }
+                                            
+                                        }
+                                        
+                                    }
+                                    
+                                }
+                               
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+        
+        } catch (Exception e) {
+        e.printStackTrace();
+        }
+        
+        return listData;
     }
 }

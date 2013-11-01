39877c109de26e91d24e8187af5bf3a3ee84fc1f
Anne Lorayne Moreira <annelorayne@gmail.com>
2013-11-01 09:03:11 -0300

    Update activity_main and imgDevice on history adapter

--- src/br/ufcg/embedded/health/servicetest/HealthDataAdapter.java
+++ src/br/ufcg/embedded/health/servicetest/HealthDataAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import br.ufcg.embedded.health.R;
 import br.ufcg.embedded.health.structures.HealthData;
@@ -49,7 +50,11 @@ public class HealthDataAdapter extends BaseAdapter {
 
         TextView tvDevice = (TextView) view.findViewById(R.id.historyDevice);
         tvDevice.setText(data.getDevice());
-
+        
+        ImageView imgDevice = (ImageView) view.findViewById(R.id.imageViewHistoryBluetooth);
+        if(data.getDevice().equals(mContext.getResources().getString(R.string.manual_data))){
+            imgDevice.setImageDrawable(mContext.getResources().getDrawable(R.drawable.blood));
+        }
         TextView tvData = (TextView) view.findViewById(R.id.historyData);
         tvData.setText(mContext.getResources().getString(R.string.pressure_sys)
                 + " " + String.valueOf(data.getSystolic().intValue())
@@ -58,6 +63,7 @@ public class HealthDataAdapter extends BaseAdapter {
                 + " " + String.valueOf(data.getDiastolic().intValue())
                 + " " + mContext.getResources().getString(R.string.unit_mmHg));
 
+        
         TextView tvAnalyzePressure = (TextView) view
                 .findViewById(R.id.historyAnalyzePressure);
         tvAnalyzePressure.setText(Handlers.analyzePressure(data.getSystolic()
